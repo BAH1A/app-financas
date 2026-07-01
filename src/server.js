8092b69fcdf575d8b248402ab9bb797d8dc5521d
JefersonW11 <jr.maio1999@gmail.com>
2026-07-01 11:55:57 -0300

    feat: add server.js, first route and .gitignore file

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-console.log("Mudei")
+const server = require('fastify')({ logger: false })
+
+
+//Rotas
+server.get('/', async function handler(request, reply) {
+    return { hello:'world'}
+})
+
+
+// Run the server!
+const porta = 9082
+server.listen({ port: porta }, (err) => {
+  if (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+  else{
+    console.log(`Servidor online, no endereço: http://localhost:${porta}`)
+  }
+})
