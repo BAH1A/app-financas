709822393416be586665cbd435deacec74fec6b8
BAHIA <jr.maio1999@gmail.com>
2026-09-14 19:53:22 -0300

    conexao com  o banco

--- database/Connection/db.js
+++ database/Connection/db.js
@@ -6,7 +6,8 @@ const { Pool} = require('pg');
 
 const conectionDB = new Pool({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    //port: process.env.POSTGRES_PORT,
+    port:2027,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD
