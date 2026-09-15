5a0919eaa447f567baecd79f6c0c0fcc3c7831fd
BAHIA <jr.maio1999@gmail.com>
2026-09-15 06:07:09 -0300

    created userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,35 @@
 const userRepository = require('../repository/userRepository.js')
 const AppError = require('../utils/errorHandler.js')
 
+class userService {
+    async createUser (userData){
+        const nome = userData.nome
+        const sobrenome = userData.sobrenome
+        const email = userData.email
+        const senha = userData.senha
+
+        //Validações
+        if (!nome || nome.length < 3){
+            throw new AppError('Nome deve ter pelo menos 3 caracteres')
+        }
+        if (!email || !email.includes('@')){
+            throw new AppError('Email inválido', 400)
+        }
+        const userToCreate = {
+            nome: nome.trim(),
+            sobrenome: sobrenome.trim(),
+            email: email.toLowerCase().trim()
+            //senha: hashedPassword torna criptografada
+        }
+        const createUser = await userRepository.createUser(userToCreate)
+        return createUser
+    }
+    async getUserById(id){
+        const user = await userRepository.getUserById(id)
+        if (!user){
+            throw new AppError('Usuário não encontrado', 404)
+        }
+        return user;
+    }
+
+} module.exports = new userRepository();
